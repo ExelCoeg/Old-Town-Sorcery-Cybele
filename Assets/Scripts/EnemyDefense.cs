c354c0c8f81713324833c689db0baea765fe78a5
4live37 <aldana.goodkids2@gmail.com>
2024-03-05 21:24:17 +0700

    nambah EnemyDefense, perubahan di EnemyHealth

--- Assets/Scripts/EnemyDefense.cs
+++ Assets/Scripts/EnemyDefense.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class EnemyDefense : MonoBehaviour
 {
+    public float maxDefense;
+    public float currentDefense;
+
     // Start is called before the first frame update
     void Start()
-    {
-        
+    {        
+        currentDefense = maxDefense;
     }
 
     // Update is called once per frame
