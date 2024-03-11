c5d91182cdf33d6557da73a796c87edc3a850036
Exel Boy Alfanso <72937148+ExelCoeg@users.noreply.github.com>
2024-03-11 12:08:45 +0700

    fixed light offset during fadeout

--- Assets/Scripts/Spells/lightOff.cs
+++ Assets/Scripts/Spells/lightOff.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 public class lightOff : MonoBehaviour
 {
     public GameObject fireballLight;
-
+    private void Start() {
+        fireballLight.transform.eulerAngles = new Vector3(0,0,0);
+    }
     void Update()
     {
-        if(GetComponent<SpriteRenderer>().sprite == null) Invoke("TriggerOff", 5);
-        
+        if(GetComponent<SpriteRenderer>().sprite == null) {
+            Invoke("TriggerOff", 5);
+            fireballLight.transform.position = Vector2.MoveTowards(fireballLight.transform.position,//
+            new Vector2(fireballLight.transform.position.x, fireballLight.transform.position.y + 1), 5 * Time.deltaTime);
+        }
     }
-
-
     void TriggerOff(){
         fireballLight.GetComponent<Animator>().SetTrigger("switch");
     }
