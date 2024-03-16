00af72cd1788778fa293d5dd7bdac4268443ece6
Exel Boy Alfanso <72937148+ExelCoeg@users.noreply.github.com>
2024-03-16 16:03:01 +0700

    created: map design

--- Assets/Scripts/Gate.cs
+++ Assets/Scripts/Gate.cs
@@ -5,11 +5,13 @@ public class Gate : MonoBehaviour
 {
     public LayerMask playerLayer;
     public GameObject craftingMenu;
-   
+    Vector2 detectPos;
+    public float detectRadius;
     // Update is called once per frame
     void Update()
     {
-        Collider2D player = Physics2D.OverlapCircle(transform.position, 1f,playerLayer);  
+        detectPos = new Vector2(transform.position.x,transform.position.y + 5);
+        Collider2D player = Physics2D.OverlapCircle(detectPos, detectRadius,playerLayer);  
         if(player != null){
             print("crafting menu on");
             craftingMenu.SetActive(true);
@@ -18,5 +20,8 @@ public class Gate : MonoBehaviour
             print("crafting menu off");
             craftingMenu.SetActive(false);
         }
+    }
+    private void OnDrawGizmos() {
+        Gizmos.DrawWireSphere(detectPos, detectRadius);
     }
 }
